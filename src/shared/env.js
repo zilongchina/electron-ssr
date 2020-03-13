8fe6c4ddc2fab0fe67a732e088701c0b61cef57c
Zilong <zilongchina@gmail.com>
2020-03-13 14:56:23 +0800

    rm

--- src/shared/env.js
+++
@@ -1,7 +0,0 @@
-
-import os from 'os'
-
-export const platform = os.platform()
-export const isWin = platform === 'win32'
-export const isMac = platform === 'darwin'
-export const isLinux = platform === 'linux'
